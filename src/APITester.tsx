8670ffddc46bde410a00af20d667358603f8ad0c
Frederik Ehmsen <Freehm01@easv365.dk>
2026-08-11 09:35:33 +0200

    added text

--- src/APITester.tsx
+++ src/APITester.tsx
@@ -1,14 +1,22 @@
-import { useRef, type FormEvent } from "react";
+import {useRef, type FormEvent, useState} from "react";
 
 export function APITester() {
 
+  const [products, setProducts] = useState([])
+
   function myButtonHasBeenClicked() {
-    
+    fetch('https://dummyjson.com/products')
+        .then(res => res.json())
+        .then(p => {
+          setProducts(p.products)
+        });
+
   }
 
   return (
     <div className="api-tester">
       <button onClick = {myButtonHasBeenClicked}>Click me to send request to some web server</button>
+      {JSON.stringify(products)}
     </div>
   );
 }
