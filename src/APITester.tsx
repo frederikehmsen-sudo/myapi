11e365a721d51a4035d8096d63a46e6eb341f52d
Frederik Ehmsen <Freehm01@easv365.dk>
2026-08-11 09:25:38 +0200

    Make the button to send the commit

---
+++ src/APITester.tsx
@@ -0,0 +1,14 @@
+import { useRef, type FormEvent } from "react";
+
+export function APITester() {
+
+  function myButtonHasBeenClicked() {
+    
+  }
+
+  return (
+    <div className="api-tester">
+      <button onClick = {myButtonHasBeenClicked}>Click me to send request to some web server</button>
+    </div>
+  );
+}
